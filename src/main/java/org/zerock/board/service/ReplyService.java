550a91dd217b1f87500d9146d817cc155d9ed027
yoon1fe <yoon1fe@naver.com>
2021-01-21 14:43:29 +0900

    [#7] Service 계층 완료

---
+++ src/main/java/org/zerock/board/service/ReplyService.java
@@ -0,0 +1,49 @@
+package org.zerock.board.service;
+
+import org.zerock.board.dto.ReplyDTO;
+import org.zerock.board.entity.Board;
+import org.zerock.board.entity.Reply;
+
+import java.util.List;
+
+public interface ReplyService {
+
+    // 댓글 등록
+    Long register(ReplyDTO replyDTO);
+
+    // 특정 게시물의 댓글 목록
+    List<ReplyDTO> getList(Long bno);
+
+    // 댓글 수정
+    void modify(ReplyDTO replyDTO);
+
+    // 댓글 삭제
+    void remove(Long rno);
+
+    // ReplyDTO를 Reply 객체로 변환 Board 객체의 처리가 수반됨
+    default Reply dtoToEntity(ReplyDTO replyDTO) {
+        Board board = Board.builder().bno(replyDTO.getBno()).build();
+
+        Reply reply = Reply.builder()
+                .rno(replyDTO.getRno())
+                .text(replyDTO.getText())
+                .replyer(replyDTO.getReplyer())
+                .board(board)
+                .build();
+
+        return reply;
+    }
+
+    // Reply 객체를 ReplyDTO 로 변환 Board 객체가 필요하지 않으면 게시물 번호만
+    default ReplyDTO entityToDto(Reply reply) {
+        ReplyDTO dto = ReplyDTO.builder()
+                .rno(reply.getRno())
+                .text(reply.getText())
+                .replyer(reply.getReplyer())
+                .regDate(reply.getRegDate())
+                .modDate(reply.getModDate())
+                .build();
+
+        return dto;
+    }
+}
